e0a5519c5c6f426d8b55c0564940ffff3c4e72e7
苏敬龙 <09067770@hucais.com>
2020-11-24 08:57:32 +0800

    test swagger-2.6.1

--- src/main/java/com/hucais/jrebeldemo/JrebelDemoApplication.java
+++ src/main/java/com/hucais/jrebeldemo/JrebelDemoApplication.java
@@ -2,9 +2,9 @@ package com.hucais.jrebeldemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.oas.annotations.EnableOpenApi;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@EnableOpenApi
+@EnableSwagger2
 @SpringBootApplication
 public class JrebelDemoApplication {
 
--- src/main/java/com/hucais/jrebeldemo/controller/JRebelController.java
+++ src/main/java/com/hucais/jrebeldemo/controller/JRebelController.java
@@ -5,6 +5,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Api(tags = "jrebel")
@@ -15,6 +16,19 @@ public class JRebelController {
     @GetMapping("/testJRebel/{id}")
     public User findById(@PathVariable Long id) {
         return new User("bbb", 21, "上海", "aaa@bbb.com");
+    }
+
+
+    @ApiOperation("testJrebel3333")
+    @GetMapping("/testJRebel3333333")
+    public Integer findById33333(@RequestParam("id3333333333") Integer id3333333333) {
+        return id3333333333;
+    }
+
+    @ApiOperation("testJRebel44444444")
+    @GetMapping("/testJRebel44444444")
+    public Integer testJRebel44444444(@RequestParam("id44444444444") Integer id44444444444) {
+        return id44444444444;
     }
 
 }
